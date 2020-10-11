350d4689b3463415e781e6ef393a73f98bc9c4b5
oreesne <72179401+oreesne@users.noreply.github.com>
2020-10-11 10:11:29 +0000

    Add files via upload

---
+++ ent.py
@@ -0,0 +1,19 @@
+from Points import *
+import re
+
+
+def toEnt(arr, **properties):
+	pts = ''
+	txt = ''
+	for name, value in properties.items():
+		pts += (f'\"{name}\" \"{value}\"\n')
+	
+	for i in range(len(arr)):
+		txt += (f'{{\n'
+						f'{pts}'
+						f'\"origin\" {arr[i].toString()}\n'
+						f'}}\n')
+		
+	return txt
+	
+
